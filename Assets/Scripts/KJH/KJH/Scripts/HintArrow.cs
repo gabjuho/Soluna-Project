6349e44defc753ae27e9c3c04115f6d86eb73dcb
MikangMark <kimjh741963@hanmail.net>
2022-09-28 17:08:54 +0900

    fix

--- Assets/Scripts/KJH/KJH/Scripts/HintArrow.cs
+++ Assets/Scripts/KJH/KJH/Scripts/HintArrow.cs
@@ -26,31 +26,15 @@ public class HintArrow : MonoBehaviour
             {
                 if(lozicManager.solve_Lozic[i] == false)
                 {
-                    hintObj.transform.LookAt(new Vector3(target[i].transform.position.x, player.transform.position.y, target[i].transform.position.z));
-                    hintObj.transform.position = RotationPos(R_CalculateRotation(player.transform.position, hintObj.transform.position, target[i].transform.position), player.transform.position, hintObj.transform.position);
+                    hintObj.transform.LookAt(new Vector3(target[i].transform.position.x, player.transform.position.y, target[i].transform.position.z), Vector3.up);
+
+
+                    hintObj.transform.position = player.transform.position + hintObj.transform.forward * 1f;
+
                     break;
                 }
             }
             
         }
     }
-    float R_CalculateRotation(Vector3 v0, Vector3 v1, Vector3 v2)
-    {
-        float temp;
-
-        temp = Mathf.Atan((v2.z - v0.z) / (v2.x - v0.x)) - Mathf.Atan((v1.z - v0.z) / (v1.x - v0.x));
-        
-        return temp;
-    }
-    Vector3 RotationPos(float radian, Vector3 v0, Vector3 v1)
-    {
-        Vector3 temp;
-
-        temp.x = Mathf.Cos(radian) * (v1.x - v0.x) - Mathf.Sin(radian) * (v1.z - v0.z);
-        temp.z = Mathf.Sin(radian) * (v1.x - v0.x) + Mathf.Cos(radian) * (v1.z - v0.z);
-        v1.x = temp.x + v0.x;
-        v1.z = temp.z + v0.z;
-        return v1;
-    }
-
 }
