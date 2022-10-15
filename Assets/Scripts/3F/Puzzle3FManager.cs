1e0e56ac3593fbd5f7fe8ccf58214ac93ea21f7c
gabjuho <gabjuho@gmail.com>
2022-10-15 11:59:46 +0900

    feat: Add 3F puzzle logic
    3층 퍼즐 로직 구현 및 에셋 추가
    
    테스트를 위해 튜토리얼 비활성화 시켜놓았으니 주석처리 다시 원상복귀 시켜야함.
    UI_Manager 17번째 줄

---
+++ Assets/Scripts/3F/Puzzle3FManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle3FManager : MonoBehaviour
+{
+    public Inventory inv;
+    public Item[] item;
+    public Light[] planet_light;
+    public Light[] crystal_light;
+
+    private int count; //현재 맞춘 행성의 개수 -> 나중에 private로 변경
+
+    void Start()
+    {
+        Invoke("AddTestItem", 0.5f);
+    }
+
+    private void AddTestItem()
+    {
+        for (int i = 0; i < 9; i++)
+            inv.AddItem(item[i]);
+    }
+
+    public bool CheckPlanet(ItemType itemType) //사용한 아이템이 맞는 행성인지 체크
+    {
+        if(item[count].itemType == itemType)
+            return true;
+
+        return false;
+    }
+
+    public void ActivePlanet() //맞은 행성 활성화
+    {
+        planet_light[count].enabled = true;
+
+        if(count > 0)
+            crystal_light[count - 1].enabled = true;
+
+        count++; //맞은 행성 개수 1증가
+
+        if (count == 9)
+            PuzzleComplete();
+    }
+
+    public void ResetPlanet() //행성 퍼즐 초기화
+    {
+        for (int i = 0; i < count; i++)
+        {
+            planet_light[i].enabled = false;
+            crystal_light[i].enabled = false;
+        }
+
+        for (int i = 0; i < count; i++)
+            inv.AddItem(item[i]);
+
+        count = 0;
+    }
+
+    public void PuzzleComplete()
+    {
+        
+    }
+}
--- Assets/Scripts/KJH/KJH/Scripts/HintArrow.cs
+++ Assets/Scripts/KJH/KJH/Scripts/HintArrow.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class HintArrow : MonoBehaviour
 {
+    private Scene scene;
     [SerializeField]
     LozicManager lozicManager;
     [SerializeField]
@@ -16,25 +18,36 @@ public class HintArrow : MonoBehaviour
     [SerializeField]
     GameObject[] target;
 
+    private void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+    }
+
     // Update is called once per frame
     void Update()
     {
         hintObj.SetActive(hintManager.on_Hint);
         if (hintManager.on_Hint)
         {
-            for(int i = 0; i < lozicManager.solve_Lozic.Length; i++)
+            if (scene.name.Equals("1F Test"))
             {
-                if(lozicManager.solve_Lozic[i] == false)
+                for (int i = 0; i < lozicManager.solve_Lozic.Length; i++)
                 {
-                    hintObj.transform.LookAt(new Vector3(target[i].transform.position.x, player.transform.position.y, target[i].transform.position.z), Vector3.up);
-                    hintObj.transform.Rotate(new Vector3(hintObj.transform.rotation.x, hintObj.transform.rotation.y + 90, hintObj.transform.rotation.z));
+                    if (lozicManager.solve_Lozic[i] == false)
+                    {
+                        hintObj.transform.LookAt(new Vector3(target[i].transform.position.x, player.transform.position.y, target[i].transform.position.z), Vector3.up);
+                        hintObj.transform.Rotate(new Vector3(hintObj.transform.rotation.x, hintObj.transform.rotation.y + 90, hintObj.transform.rotation.z));
 
-                    hintObj.transform.position = player.transform.position + -hintObj.transform.right * 1f;
+                        hintObj.transform.position = player.transform.position + -hintObj.transform.right * 1f;
 
-                    break;
+                        break;
+                    }
                 }
             }
-            
+            else if(scene.name.Equals("3F"))
+            {
+
+            }
         }
     }
 }
